e82f6c4f297da45795491c1fe36f0fdfbc3c1e29
Nomikos <62618905+NSkyllas@users.noreply.github.com>
2024-11-12 16:44:47 +0100

    Update DOY_model_script.py

--- DOY_model_script.py
+++ DOY_model_script.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
+#import cartopy.crs as ccrs
+#import cartopy.feature as cfeature
 import streamlit as st
 
 
@@ -21,12 +21,18 @@ for f in ['dsn5', 'dsf5', 'dsn2', 'dsf2', 'dsn1', 'dsf1', 'dsh', 'dss']:
 weights = np.cos(np.deg2rad(dss.lat))
 weights.name = "weights"
 
-fig, ax = plt.subplots(ncols = 5, nrows = 2, figsize = (20, 6), subplot_kw={'projection': ccrs.NorthPolarStereo()})
+#fig, ax = plt.subplots(ncols = 5, nrows = 2, figsize = (20, 6), subplot_kw={'projection': ccrs.NorthPolarStereo()})
+#ax = ax.flatten()
+#for i, m in enumerate(model_list):
+#    dsh.doy.sel(model = m).mean(['time']).plot(ax = ax[i], vmin = 100, vmax = 210, transform = ccrs.PlateCarree())
+#    ax[i].coastlines()
+#    ax[i].set_title(m)
+#plt.suptitle('Historical, mean of 2000 - 2013', fontsize = 16)
+
+fig, ax = plt.subplots(ncols = 5, nrows = 2, figsize = (20, 6))
 ax = ax.flatten()
 for i, m in enumerate(model_list):
-    dsh.doy.sel(model = m).mean(['time']).plot(ax = ax[i], vmin = 100, vmax = 210, transform = ccrs.PlateCarree())
-    ax[i].coastlines()
-    ax[i].set_title(m)
+    dsh.doy.sel(model = m).mean(['time']).plot(ax = ax[i], vmin = 100, vmax = 210)
 plt.suptitle('Historical, mean of 2000 - 2013', fontsize = 16)
 
 st.pyplot(fig)
