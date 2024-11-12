8af952411b0e342698f6e9a463b2b80301ad5ca8
Nomikos <62618905+NSkyllas@users.noreply.github.com>
2024-11-12 17:09:40 +0100

    Update DOY_model_script.py

--- DOY_model_script.py
+++ DOY_model_script.py
@@ -54,5 +54,32 @@ elif (scenario == 'SSP-2.45') & (period == 'Near future (2040-2060)'):
     plt.tight_layout()
     st.pyplot(fig)
 
+elif (scenario == 'SSP-5.85') & (period == 'Near future (2080-2100)'):
+    fig, ax = plt.subplots(ncols = 5, nrows = 2, figsize = (20, 6))
+    ax = ax.flatten()
+    for i, m in enumerate(model_list):
+        dsn2.doy.sel(model = m).mean(['time']).plot(ax = ax[i], vmin = 100, vmax = 210)
+    plt.suptitle('SSP-5.85, mean of 2080 - 2100 (near future)', fontsize = 16)
+    plt.tight_layout()
+    st.pyplot(fig)
+
+elif (scenario == 'SSP-5.85') & (period == 'Near future (2040-2060)'):
+    fig, ax = plt.subplots(ncols = 5, nrows = 2, figsize = (20, 6))
+    ax = ax.flatten()
+    for i, m in enumerate(model_list):
+        dsn2.doy.sel(model = m).mean(['time']).plot(ax = ax[i], vmin = 100, vmax = 210)
+    plt.suptitle('SSP-5.85, mean of 2040 - 2060 (near future)', fontsize = 16)
+    plt.tight_layout()
+    st.pyplot(fig)
+
+elif (scenario == 'SSP-5.85') & (period == 'Far future (2080-2100)'):
+    fig, ax = plt.subplots(ncols = 5, nrows = 2, figsize = (20, 6))
+    ax = ax.flatten()
+    for i, m in enumerate(model_list):
+        dsn2.doy.sel(model = m).mean(['time']).plot(ax = ax[i], vmin = 100, vmax = 210)
+    plt.suptitle('SSP-5.85, mean of 2080 - 2100 (far future)', fontsize = 16)
+    plt.tight_layout()
+    st.pyplot(fig)
+
 else:
     st.write('This combination is not available')
