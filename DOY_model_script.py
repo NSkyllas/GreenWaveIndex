ba2d18807e7dd7e9b16aa3c16fe40deb59ea0774
Nomikos <62618905+NSkyllas@users.noreply.github.com>
2024-11-12 16:59:29 +0100

    Update DOY_model_script.py

--- DOY_model_script.py
+++ DOY_model_script.py
@@ -42,5 +42,5 @@ ax = ax.flatten()
 for i, m in enumerate(model_list):
     dsf5.doy.sel(model = m).mean(['time']).plot(ax = ax[i], vmin = 100, vmax = 210)
 plt.suptitle('SSP-5.86, mean of 2080 - 2100 (far future)', fontsize = 16)
-
+plt.tight_layout()
 st.pyplot(fig2)
